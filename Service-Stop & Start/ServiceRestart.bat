07b16e9312430e130c3fa823271d930875188f8b
kurubakeshava <kurubakeshava0@gmail.com>
2024-12-17 11:01:57 +0530

    service start & stop batch files

---
+++ Service-Stop & Start/ServiceRestart.bat
@@ -0,0 +1,11 @@
+NET START ContentverseService
+sleep 20
+NET START ContentverseStorage
+NET START ContentverseWorkflow
+NET START ContentverseRetention
+NET START ContentverseNotification
+NET START "ContentverseContent Sentinel"
+NET START "Contentverse AIP Service"
+NET START "Contentverse Indexer Service"
+
+
---
+++ Service-Stop & Start/ServiceStop.bat
@@ -0,0 +1,8 @@
+NET STOP "Contentverse AIP Service"
+NET STOP "Contentverse Indexer Service"
+NET STOP ContentverseNotification
+NET STOP ContentverseRetention
+NET STOP "ContentverseContent Sentinel"
+NET STOP ContentverseWorkflow
+NET STOP ContentverseStorage
+NET STOP ContentverseService
